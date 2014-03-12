3e4046fc8506320a00e173f12cf432416d5f6da9
Eric Bréchemier <medea@eric.brechemier.name>
2014-03-12 14:14:53 +0100

    Collect inputs and apply filter on equality of matching field
    The current filters perform an exact match, comparing the value
    of the filter with the value of the field of the same name in
    each record.

--- code/filter.js
+++ code/filter.js
@@ -1,14 +1,65 @@
-within("projetmedea.fr", function(publish, subscribe){
+within("projetmedea.fr", function(publish, subscribe, get){
 
   var
-    form = document.getElementById('data-filters');
+    forEach = this.forEach,
+    no = this.no,
+    form = document.getElementById('data-filters'),
+    fieldNames = [],
+    fieldPositions = {};
+
+  function initFieldPositions(){
+    forEach(fieldNames, function(fieldName, fieldPosition){
+      fieldPositions[fieldName] = fieldPosition;
+    });
+  }
+
+  // Get the offset of a field
+  function getFieldPosition(name){
+    return fieldPositions[name];
+  }
+
+  function collectFilters(){
+    var filters = [];
+    forEach(fieldNames, function(fieldName){
+      var input = form[fieldName];
+      if ( !no(input) && !no(input.nodeType) && input.value !== '' ){
+        filters.push(input);
+      }
+    });
+    publish("filters", filters);
+  }
+
+  function filter(data, filters){
+    var selected = [];
+    forEach(data, function(record, position){
+      if ( position === 0 ) {
+        selected.push(record); // always keep header
+        return;
+      }
+      var isRejected = forEach(filters, function(filter){
+        return record[getFieldPosition(filter.name)] !== filter.value;
+      });
+      if ( !isRejected ) {
+        selected.push(record);
+      }
+    });
+    return selected;
+  }
 
   form.onsubmit = function(){
+    collectFilters();
     return false; // prevent submission to server (reloads the page)
   };
 
   // publish initial data (no filter applied)
   subscribe("data", function(data){
+    fieldNames = data[0];
+    initFieldPositions();
     publish("filtered-data", data);
+  });
+
+  subscribe("filters", function(filters){
+    var data = get('data');
+    publish("filtered-data", filter(data,filters) );
   });
 });
