e916ca9c44cdbd44c06b5e5cb2a370619c50b32b
Eric Bréchemier <medea@eric.brechemier.name>
2014-03-12 12:25:36 +0100

    Allow any order for "data" and "filtered-data" events
    When this module is loaded before "filter.js", it receives "data" event
    first, before "filter.js" publishes "filtered-data" event. But when this
    module is loaded after "filter.js", it receives "filtered-data" event
    before "data".

--- code/count-selected-percentage.js
+++ code/count-selected-percentage.js
@@ -1,15 +1,34 @@
 within("projetmedea.fr", function(publish, subscribe){
 
-  var total = null;
+  var
+    total = null,
+    selected = null,
+    display = document.getElementById('selected-records-percentage');
+
+  // Count actual records (without header) in given list
+  function count(data){
+    return data.length - 1; // do not count header record
+  }
+
+  // Compute the percentage that the part represents in the whole
+  function percentage(part, whole){
+    return Math.round(part / whole * 100);
+  }
+
+  function update(){
+    if (total===null || selected===null) {
+      return;
+    }
+    display.innerHTML = percentage(selected, total);
+  }
 
   subscribe("data", function(data){
-    total = data.length - 1; // do not count header record
+    total = count(data);
+    update();
   });
 
   subscribe("filtered-data", function(data){
-    var selected = data.length - 1; // do not count header row
-    document
-      .getElementById('selected-records-percentage')
-      .innerHTML = Math.round(selected / total * 100);
+    selected = count(data);
+    update();
   });
 });
