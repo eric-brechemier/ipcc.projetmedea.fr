34a71c8c24dac521631411dde4e413a0647675e9
Eric Bréchemier <medea@eric.brechemier.name>
2014-03-12 12:48:23 +0100

    Prevent form submission from JavaScript

--- code/filter.js
+++ code/filter.js
@@ -1,5 +1,12 @@
 within("projetmedea.fr", function(publish, subscribe){
 
+  var
+    form = document.getElementById('data-filters');
+
+  form.onsubmit = function(){
+    return false; // prevent submission to server (reloads the page)
+  };
+
   // publish initial data (no filter applied)
   subscribe("data", function(data){
     publish("filtered-data", data);
