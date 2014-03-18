c949d7a673b6cb3efb457c97136bba2b2dc66b5c
Eric Bréchemier <medea@eric.brechemier.name>
2014-03-18 19:41:12 +0100

    Add or() to utility functions

--- code/utils.js
+++ code/utils.js
@@ -52,6 +52,26 @@ within("projetmedea.fr", function() {
     return value === null || value === undef;
   }
 
+  // CC0 - https://raw.github.com/eric-brechemier/nadasurf/master/or.js
+  /*
+    Get a default value when given value is null or undefined
+
+    Parameters:
+      a - any, the value to check
+      b - any, the default value
+
+    Returns:
+      any, the default value when the value is null or undefined,
+      the value itself otherwise.
+
+    Dependency:
+      nada/no.js
+  */
+  function or( a, b ) {
+    return no( a )? b: a;
+  }
+
   this.forEach = forEach;
   this.no = no;
+  this.or = or;
 });
