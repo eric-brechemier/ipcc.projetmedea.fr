ff3b678474ac7b845bc1ede110c87018b7aa9b63
Eric Bréchemier <medea@eric.brechemier.name>
2014-03-18 19:52:54 +0100

    Add map() to utility functions

--- code/utils.js
+++ code/utils.js
@@ -36,6 +36,30 @@ within("projetmedea.fr", function() {
     return isBreak;
   }
 
+  // CC0 - https://raw.github.com/eric-brechemier/nadasurf/master/map.js
+  /*
+    Apply a function to all the elements in a list
+
+    Parameters:
+      array - array, the list of items to process
+      operation - function( value, offset ), the function to apply to each item,
+                  called with the value and offset of each item. The result of
+                  the operation is stored at the same offset in result array.
+
+    Returns:
+      array, the list of results of the operation applied to each item
+      of the given array.
+  */
+  function map( array, operation ) {
+    var result = Array( array.length );
+
+    forEach( array, function( item, i ) {
+      result[ i ] = operation( item, i );
+    });
+
+    return result;
+  }
+
   // CC0 - https://raw.github.com/eric-brechemier/nada/master/no.js
   /*
     Check whether given value is null or undefined
@@ -72,6 +96,7 @@ within("projetmedea.fr", function() {
   }
 
   this.forEach = forEach;
+  this.map = map;
   this.no = no;
   this.or = or;
 });
