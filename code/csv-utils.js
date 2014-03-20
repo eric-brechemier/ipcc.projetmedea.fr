7a748b10c6eee192c783c83a4cfc750569c8f7cc
Eric Bréchemier <medea@eric.brechemier.name>
2014-03-20 17:35:16 +0100

    Move printData() to CSV Utils for reuse

--- code/csv-utils.js
+++ code/csv-utils.js
@@ -1,9 +1,49 @@
 within("projetmedea.fr", function(){
 
+  var
+    forEach = this.forEach;
+
   // Count actual records (without header) in given list
   function countData(records){
     return records.length - 1; // do not count header record
   }
 
+  // Print records as a string in CSV format
+  function printData(records) {
+    var
+      csv = "",
+      lastRecord = records.length - 1;
+
+    forEach(records, function(record, recordPosition){
+      var
+        lastField = record.length - 1,
+        lastItem;
+      forEach(record, function(field, fieldPosition){
+        if ( typeof field === 'string' ) {
+          csv += '"' + field.replace('"','""') + '"';
+        } else { // array
+          lastItem = field.length - 1;
+          csv += '[';
+          forEach(field, function(item, itemPosition){
+            csv += item;
+            if ( itemPosition < lastItem ) {
+              csv += '|';
+            }
+          });
+          csv += ']';
+        }
+        if (fieldPosition < lastField) {
+          csv += ',';
+        }
+      });
+      if (recordPosition < lastRecord) {
+        csv += '\n';
+      }
+    });
+
+    return csv;
+  }
+
   this.countData = countData;
+  this.printData = printData;
 });
--- code/print-selected-authors.js
+++ code/print-selected-authors.js
@@ -1,43 +1,10 @@
 within("projetmedea.fr", function(publish, subscribe){
-
   var
-    forEach = this.forEach,
-    output = document.getElementById("selected-authors-csv-output");
+    printData = this.printData;
 
-  function printData(records) {
-    var
-      csv = "",
-      lastRecord = records.length - 1;
-
-    forEach(records, function(record, recordPosition){
-      var
-        lastField = record.length - 1,
-        lastItem;
-      forEach(record, function(field, fieldPosition){
-        if ( typeof field === 'string' ) {
-          csv += '"' + field.replace('"','""') + '"';
-        } else { // array
-          lastItem = field.length - 1;
-          csv += '[';
-          forEach(field, function(item, itemPosition){
-            csv += item;
-            if ( itemPosition < lastItem ) {
-              csv += '|';
-            }
-          });
-          csv += ']';
-        }
-        if (fieldPosition < lastField) {
-          csv += ',';
-        }
-      });
-      if (recordPosition < lastRecord) {
-        csv += '\n';
-      }
-    });
-
-    output.innerHTML = csv;
-  }
-
-  subscribe("selected-authors", printData);
+  subscribe("selected-authors", function(selectedAuthors){
+    document
+      .getElementById("selected-authors-csv-output")
+      .innerHTML = printData(selectedAuthors);
+  });
 });
