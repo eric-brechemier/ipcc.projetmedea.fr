76841e52616966abc721c3eb81815b5bf9c6c24e
Eric Bréchemier <medea@eric.brechemier.name>
2014-03-20 16:27:05 +0100

    Bug fix: do not count header row in categories

--- code/count-categories.js
+++ code/count-categories.js
@@ -3,6 +3,6 @@ within("projetmedea.fr", function(publish, subscribe){
   subscribe("categories", function(categories){
     document
       .getElementById('total-categories-count')
-      .innerHTML = categories.length;
+      .innerHTML = categories.length - 1; // do not count header row
   });
 });
