342a06b7264016e06bdd74fa29d0c585f7a2ddb3
Eric Bréchemier <medea@eric.brechemier.name>
2014-03-20 16:03:24 +0100

    Rename 'filed-names' property to 'author-field-names'

--- code/filter-authors.js
+++ code/filter-authors.js
@@ -43,7 +43,7 @@ within("projetmedea.fr", function(publish, subscribe, get, set){
     var
       FILTER_PREFIX = 'filter-',
       filters = [],
-      fieldNames = get('field-names');
+      fieldNames = get('author-field-names');
     forEach(fieldNames, function(fieldName){
       var input = form[FILTER_PREFIX+fieldName];
       if ( !no(input) && !no(input.nodeType) && input.value !== '' ){
@@ -96,12 +96,12 @@ within("projetmedea.fr", function(publish, subscribe, get, set){
   };
 
   subscribe("authors", function(data){
-    publish('field-names', data[0]);
+    publish('author-field-names', data[0]);
     // apply initial filters
     applyFilters();
   });
 
-  subscribe("field-names", function(fieldNames){
+  subscribe("author-field-names", function(fieldNames){
     initFieldPositions(fieldNames);
   });
 
