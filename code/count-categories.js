b0d9a0695f47efc8aa434b07292d1a9dcd7ebe68
Eric Bréchemier <medea@eric.brechemier.name>
2014-03-20 15:45:00 +0100

    Add module to display the total number of current categories

---
+++ code/count-categories.js
@@ -0,0 +1,8 @@
+within("projetmedea.fr", function(publish, subscribe){
+
+  subscribe("categories", function(categories){
+    document
+      .getElementById('total-categories-count')
+      .innerHTML = categories.length;
+  });
+});
