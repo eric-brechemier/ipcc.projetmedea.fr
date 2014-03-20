ce589fbb172401fc228f91b0e0f672b4b8fba82a
Eric Bréchemier <medea@eric.brechemier.name>
2014-03-20 13:58:57 +0100

    Add module which keeps track of the selected grouping criterion

---
+++ code/group-by-category.js
@@ -0,0 +1,27 @@
+within("projetmedea.fr", function(publish, subscribe){
+
+  var
+    forEach = this.forEach,
+    groupingForm = document.getElementById('data-grouping'),
+    groupingOptions = groupingForm['group-by'];
+
+  function getSelectedValue(inputs){
+    var selectedValue = null;
+    forEach(inputs, function(input){
+      if ( input.checked ){
+        selectedValue = input.value;
+        return true;
+      }
+    });
+    return selectedValue;
+  }
+
+  function updateGroupingCategory(){
+    publish('group-by', getSelectedValue(groupingOptions) );
+  }
+
+  forEach(groupingOptions, function(input){
+    input.onchange = updateGroupingCategory;
+  });
+  updateGroupingCategory();
+});
