f64f4e41f8c5bf4e7a14628cd1cc653d35ee320d
Eric Bréchemier <medea@eric.brechemier.name>
2014-03-21 17:42:46 +0100

    Wrap the table layout in a chart
    The goal is to get the width and height of the chart computed
    at the top level of the layout.
    
    Also, some layouts will define multiple charts that may be rendered
    separately depending on the width of the browser window.

--- layout/working-group-categories.js
+++ layout/working-group-categories.js
@@ -1,72 +1,77 @@
 within("projetmedea.fr", function(publish){
   publish("layout/working-group-categories",function(){
     return [
-      ["table-layout","column1+2","column3"],
-      ["row1+2+3",
+      ["chart","width"],
+      ["height",
         [
-          ["table-layout",
-            "column1",
-            "column2"
-          ],
-          ["row1",
-            {
-              name: 'WG1',
-              shape: 'circle',
-              valign: 'bottom',
-              align: 'right',
-              color: '#FF0000'
-            },
-            {
-              name: 'WG1+3',
-              shape: 'vline',
-              valign: 'middle',
-              align: 'center',
-              color: '#FF00FF'
-            }
-          ],
-          ["row2",
-            {
-              name: 'WG1+2',
-              shape: 'line',
-              valign: 'middle',
-              align: 'center',
-              color: '#FFFF00'
-            },
-            {
-              name: 'WG1+2+3',
-              shape: 'circle',
-              valign: 'middle',
-              align: 'center',
-              color: '#FFFFFF'
-            }
-          ],
-          ["row3",
-            {
-              name: 'WG2',
-              shape: 'circle',
-              valign: 'top',
-              align: 'right',
-              color: '#00FF00'
-            },
-            {
-              name: 'WG2+3',
-              shape: 'vline',
-              valign: 'middle',
-              align: 'center',
-              color: '#00FFFF'
-            }
-          ]
-        ],
-        [
-          ["table-layout", "column3"],
+          ["table-layout","column1+2","column3"],
           ["row1+2+3",
-            {
-              name: 'WG3',
-              shape: 'circle',
-              valign: 'middle',
-              align: 'left',
-              color: '#0000FF'
-            }
+            [
+              ["table-layout",
+                "column1",
+                "column2"
+              ],
+              ["row1",
+                {
+                  name: 'WG1',
+                  shape: 'circle',
+                  valign: 'bottom',
+                  align: 'right',
+                  color: '#FF0000'
+                },
+                {
+                  name: 'WG1+3',
+                  shape: 'vline',
+                  valign: 'middle',
+                  align: 'center',
+                  color: '#FF00FF'
+                }
+              ],
+              ["row2",
+                {
+                  name: 'WG1+2',
+                  shape: 'line',
+                  valign: 'middle',
+                  align: 'center',
+                  color: '#FFFF00'
+                },
+                {
+                  name: 'WG1+2+3',
+                  shape: 'circle',
+                  valign: 'middle',
+                  align: 'center',
+                  color: '#FFFFFF'
+                }
+              ],
+              ["row3",
+                {
+                  name: 'WG2',
+                  shape: 'circle',
+                  valign: 'top',
+                  align: 'right',
+                  color: '#00FF00'
+                },
+                {
+                  name: 'WG2+3',
+                  shape: 'vline',
+                  valign: 'middle',
+                  align: 'center',
+                  color: '#00FFFF'
+                }
+              ]
+            ],
+            [
+              ["table-layout", "column3"],
+              ["row1+2+3",
+                {
+                  name: 'WG3',
+                  shape: 'circle',
+                  valign: 'middle',
+                  align: 'left',
+                  color: '#0000FF'
+                }
+              ]
+            ]
           ]
         ]
       ]
