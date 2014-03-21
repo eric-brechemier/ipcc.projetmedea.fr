bf51e0e82c21bde8f7d2342493593068c8d2fb21
Eric Bréchemier <medea@eric.brechemier.name>
2014-03-21 15:55:52 +0100

    Compute the dimensions of each group based on number of authors within
    This completes the step H of the algorithm:
    
      H) For each group with a circular shape, look up the maximum dimension
         for the number of tiles in the group. The length of linear shapes is
         the number of tiles in the group:
    
         - the height of vertical lines equals the number of tiles,
           their width is 1
    
         - the width of horizontal lines equals the number of tiles,
           their height is 1

--- code/layout.js
+++ code/layout.js
@@ -1,9 +1,88 @@
 within("projetmedea.fr", function(publish, subscribe, get){
+  var
+    getExpectedCircleWidth = this.getExpectedCircleWidth,
+    forEach = this.forEach,
+    CATEGORY_NAME = 0,
+    CATEGORY_TOTAL_AUTHORS_SELECTED = 2,
+    TILE_CIRCLE_WIDTH = 0;
 
-  function resetLayout(){
-    var category = get("group-by");
-    publish("group-layout", get("layout/"+category)() );
+  function getTotalAuthorsSelectedInGroup(groupName){
+    var
+      selectedAuthors = 0,
+      selectedCategories = get("selected-categories");
+    forEach(selectedCategories, function(selectedCategory){
+      if ( selectedCategory[CATEGORY_NAME] === groupName ){
+        selectedAuthors = selectedCategory[CATEGORY_TOTAL_AUTHORS_SELECTED];
+        return true;
+      }
+    });
+    return selectedAuthors;
   }
 
-  subscribe("selected-categories", resetLayout);
+  function getCircleWidth(tilesCount){
+    var
+      expectedWidth = getExpectedCircleWidth(tilesCount),
+      sequence;
+
+    if ( expectedWidth %2 === 0 ){
+      sequence = get("even-circle-sequence");
+    } else {
+      sequence = get("odd-circle-sequence");
+    }
+    return sequence[tilesCount][TILE_CIRCLE_WIDTH];
+  }
+
+  function setCellDimensions(cell){
+    var
+      tilesCount = getTotalAuthorsSelectedInGroup(cell.name);
+
+    switch (cell.shape) {
+      case 'circle':
+        cell.width = getCircleWidth(tilesCount);
+        cell.height = cell.width;
+        break;
+      case 'line':
+        cell.width = tilesCount;
+        cell.height = 1;
+        break;
+      case 'vline': // vertical line
+        cell.width = 1;
+        cell.height = tilesCount;
+        break;
+    }
+  }
+
+  function setLayoutDimensions(tableLayout){
+    var tableHeaderRow;
+
+    forEach(tableLayout, function(row, rowPosition){
+      if ( rowPosition === 0 ){
+        tableHeaderRow = row;
+        return;
+      }
+      forEach(row, function(cell, cellPosition){
+        if ( cellPosition === 0 ){
+          return; // skip row header cell
+        }
+
+        // Check whether a cell contains a group
+        if ( typeof cell.shape === 'string' ){
+          setCellDimensions(cell);
+        } else { // the cell contains a nested table layout
+          setLayoutDimensions(cell);
+        }
+      });
+    });
+  }
+
+  function updateLayout(selectedCategories){
+    var
+      category = get("group-by"),
+      layout = get("layout/"+category)();
+
+    setLayoutDimensions(layout);
+    publish("group-layout", layout);
+  }
+
+  subscribe("selected-categories", updateLayout);
 });
