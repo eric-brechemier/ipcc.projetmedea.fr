b495d0ebe27d37eb4edee02b237f3355b7b6f27a
Eric Bréchemier <medea@eric.brechemier.name>
2014-03-21 14:07:54 +0100

    Wrap table layouts for working groups in a parent table layout

--- layout/working-group-categories.js
+++ layout/working-group-categories.js
@@ -1,71 +1,74 @@
 within("projetmedea.fr", function(publish){
   publish(
     "layout/working-group-categories",
-    ["chart",
-      [
-        ["table-layout",
-          "column1",
-          "column2"
+    [
+      ["table-layout","column1+2","column3"],
+      ["row1+2+3",
+        [
+          ["table-layout",
+            "column1",
+            "column2"
+          ],
+          ["row1",
+            {
+              name: 'WG1',
+              shape: 'circle',
+              valign: 'bottom',
+              align: 'right',
+              color: '#FF0000'
+            },
+            {
+              name: 'WG1+3',
+              shape: 'vline',
+              valign: 'middle',
+              align: 'center',
+              color: '#FF00FF'
+            }
+          ],
+          ["row2",
+            {
+              name: 'WG1+2',
+              shape: 'line',
+              valign: 'middle',
+              align: 'center',
+              color: '#FFFF00'
+            },
+            {
+              name: 'WG1+2+3',
+              shape: 'circle',
+              valign: 'middle',
+              align: 'center',
+              color: '#FFFFFF'
+            }
+          ],
+          ["row3",
+            {
+              name: 'WG2',
+              shape: 'circle',
+              valign: 'top',
+              align: 'right',
+              color: '#00FF00'
+            },
+            {
+              name: 'WG2+3',
+              shape: 'vline',
+              valign: 'middle',
+              align: 'center',
+              color: '#00FFFF'
+            }
+          ]
         ],
-        ["row1",
-          {
-            name: 'WG1',
-            shape: 'circle',
-            valign: 'bottom',
-            align: 'right',
-            color: '#FF0000'
-          },
-          {
-            name: 'WG1+3',
-            shape: 'vline',
-            valign: 'middle',
-            align: 'center',
-            color: '#FF00FF'
-          }
-        ],
-        ["row2",
-          {
-            name: 'WG1+2',
-            shape: 'line',
-            valign: 'middle',
-            align: 'center',
-            color: '#FFFF00'
-          },
-          {
-            name: 'WG1+2+3',
-            shape: 'circle',
-            valign: 'middle',
-            align: 'center',
-            color: '#FFFFFF'
-          }
-        ],
-        ["row3",
-          {
-            name: 'WG2',
-            shape: 'circle',
-            valign: 'top',
-            align: 'right',
-            color: '#00FF00'
-          },
-          {
-            name: 'WG2+3',
-            shape: 'vline',
-            valign: 'middle',
-            align: 'center',
-            color: '#00FFFF'
-          }
-        ]
-      ],
-      [
-        ["table-layout", "column3"],
-        ["row1+2+3",
-          {
-            name: 'WG3',
-            shape: 'circle',
-            valign: 'middle',
-            align: 'left',
-            color: '#0000FF'
-          }
+        [
+          ["table-layout", "column3"],
+          ["row1+2+3",
+            {
+              name: 'WG3',
+              shape: 'circle',
+              valign: 'middle',
+              align: 'left',
+              color: '#0000FF'
+            }
+          ]
         ]
       ]
     ]
