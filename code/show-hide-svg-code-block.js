7a61779f71fe0a369c0a0cd211000fd7d26fa0b8
Eric Bréchemier <medea@eric.brechemier.name>
2014-03-28 14:03:26 +0100

    Extract function showHide() to new module interactivity-utils.js

--- code/show-hide-svg-code-block.js
+++ code/show-hide-svg-code-block.js
@@ -1,20 +1,4 @@
 within("projetmedea.fr", function(publish, subscribe){
-  var
-    showButton = document.getElementById("show-svg-code-block"),
-    hideButton = document.getElementById("hide-svg-code-block"),
-    presenterBlock = document.getElementById("svg-code-block-presenter"),
-    codeBlock = document.getElementById("svg-code-block");
 
-  function showSvgCode(){
-    presenterBlock.className = "hidden";
-    codeBlock.className = "";
-  }
-
-  function hideSvgCode(){
-    presenterBlock.className = "";
-    codeBlock.className = "hidden";
-  }
-
-  showButton.onclick = showSvgCode;
-  hideButton.onclick = hideSvgCode;
+  this.showHide("svg-code-block");
 });
