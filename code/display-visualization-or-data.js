3910f5fba8fdc74a9da99690f15a6f8ad6ed6786
Eric Bréchemier <medea@eric.brechemier.name>
2014-05-13 18:25:20 +0200

    Add warning with explanation of expected markup for display selection

--- code/display-visualization-or-data.js
+++ code/display-visualization-or-data.js
@@ -9,6 +9,12 @@ within("projetmedea.fr", function() {
   function selectDisplay() {
     var
       selectedValue = displaySelection.value,
+      // WARNING: no text must be left between options in markup
+      // like in following example
+      // <select ...
+      //   ><option ... >...</option
+      //   ><option ... >...</option
+      // ></select>
       options = displaySelection.childNodes;
 
     forEach(options, function(option) {
