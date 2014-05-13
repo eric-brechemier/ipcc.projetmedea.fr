59f18bd0f4bd4209d5f4ef9758a421eb40825967
Eric Bréchemier <medea@eric.brechemier.name>
2014-05-13 18:35:20 +0200

    Apply the initial selection (selected value remains on refresh)
    I had to move the script definition after the definition of each block,
    which were otherwise undefined during the initial invocation.

--- code/display-visualization-or-data.js
+++ code/display-visualization-or-data.js
@@ -1,35 +1,7 @@
 within("projetmedea.fr", function() {
 
-  var
-    forEach = this.forEach,
-    showBlock = this.showBlock,
-    hideBlock = this.hideBlock,
-    displaySelection = document.getElementById("display-selection");
-
-  function selectDisplay() {
-    var
-      selectedValue = displaySelection.value,
-      // WARNING: no text must be left between options in markup
-      // like in following example
-      // <select ...
-      //   ><option ... >...</option
-      //   ><option ... >...</option
-      // ></select>
-      options = displaySelection.childNodes;
-
-    forEach(options, function(option) {
-      var
-        value = option.value,
-        block = document.getElementById(value);
-
-      if ( value === selectedValue ) {
-        showBlock(block);
-      } else {
-        hideBlock(block);
-      }
-    });
-  }
-
-  displaySelection.onchange = selectDisplay;
+  this.selectBlockToShow(
+    document.getElementById("display-selection")
+  );
 
 });
--- code/interactivity-utils.js
+++ code/interactivity-utils.js
@@ -1,6 +1,7 @@
 within("projetmedea.fr", function(){
   var
     or = this.or,
+    forEach = this.forEach,
 
     CSS_CLASS_HIDDEN = "hidden";
 
@@ -43,8 +44,39 @@ within("projetmedea.fr", function(){
     hideButton.onclick = hideCodeBlock;
   }
 
+  function selectBlockToShow( displaySelection ) {
+
+    function selectDisplay() {
+      var
+        selectedValue = displaySelection.value,
+        // WARNING: no text must be left between options in markup
+        // like in following example
+        // <select ...
+        //   ><option ... >...</option
+        //   ><option ... >...</option
+        // ></select>
+        options = displaySelection.childNodes;
+
+      forEach(options, function(option) {
+        var
+          value = option.value,
+          block = document.getElementById(value);
+
+        if ( value === selectedValue ) {
+          showBlock(block);
+        } else {
+          hideBlock(block);
+        }
+      });
+    }
+
+    selectDisplay();
+    displaySelection.onchange = selectDisplay;
+  }
+
   this.showBlock = showBlock;
   this.hideBlock = hideBlock;
   this.showHide = showHide;
+  this.selectBlockToShow = selectBlockToShow;
 
 });
