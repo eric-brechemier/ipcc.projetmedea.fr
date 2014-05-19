bbb98818850ab5d6dfbbbfe3def946f31c73203e
Eric Bréchemier <medea@eric.brechemier.name>
2014-05-19 18:22:33 +0200

    Prepare to compute predictive filters in filter-authors

--- code/filter-authors.js
+++ code/filter-authors.js
@@ -1,12 +1,17 @@
 within("projetmedea.fr", function(publish, subscribe, get){
 
   var
+    no = this.no,
+    or = this.or,
     forEach = this.forEach,
     alwaysTrue = this.alwaysTrue,
     form = document.getElementById("filters"),
 
     // offset of the column with author identifier in each author record
-    AUTHOR_ID = 0;
+    AUTHOR_ID = 0,
+
+    // offset of the column with the list of authors in a category record
+    CATEGORY_AUTHORS = 2;
 
   form.onsubmit = function(){
     return false; // prevent submission to server (reloads the page)
@@ -28,10 +33,27 @@ within("projetmedea.fr", function(publish, subscribe, get){
         selectedFlags[authorId] = true;
       }
     });
+
     publish("selected-authors", selected);
+    /* Work in progress
+    publish("selected-authors-prediction", function(category, filterName) {
+      var
+        filterValue = category[CATEGORY_NAME],
+        totalAuthors = totalAuthorsByFilter[filterName][filterValue];
+      return or(totalAuthors, 0);
+    });
+    */
     publish("selected-author-check", function(authorId){
       return selectedFlags[authorId] === true;
     });
+  }
+
+  function getTotalAuthorsInCategory(category) {
+    if ( no(category) ) {
+      return 0;
+    }
+    var authors = category[CATEGORY_AUTHORS];
+    return authors.length;
   }
 
   function applyFilters() {
@@ -43,6 +65,7 @@ within("projetmedea.fr", function(publish, subscribe, get){
     if ( activeFilterList.length === 0 ) {
       // shortcut: select all authors
       publish("selected-authors", authors);
+      publish("selected-authors-prediction", getTotalAuthorsInCategory);
       publish("selected-author-check", alwaysTrue);
       return;
     }
