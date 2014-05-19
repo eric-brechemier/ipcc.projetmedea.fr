712022bd1ea0033800e76a3dec60452410a7ab03
Eric Bréchemier <medea@eric.brechemier.name>
2014-05-19 14:45:55 +0200

    Move constants from combine-active-filters to filter-constants
    The goal is to share these constants with a new module,
    prepare-predictive-filters.

--- code/combine-active-filters.js
+++ code/combine-active-filters.js
@@ -4,44 +4,14 @@ within("projetmedea.fr", function(publish, subscribe, get) {
     no = this.no,
     alwaysTrue = this.alwaysTrue,
 
-    // offset of the field with the list of author contribution codes
-    // in each author record
-    AUTHOR_CONTRIBUTIONS = 3,
-
-    // sequence of the names of filters
-    // that make up the contribution code
-    CONTRIBUTION_CODE_FILTERS = [
-      "assessment-report-categories",
-      "working-group-categories",
-      "role-categories",
-      "country-categories",
-      "institution-type-categories",
-      "institution-categories"
-    ],
-
-    // name of the filter for the total number of contributions
-    // corresponding to the contribution code
-    TOTAL_CONTRIBUTIONS_FILTER = "total-contributions-categories",
-
-    // part of the filter expression for a filter that matches any value
-    // (matches all characters up to the next filter separator)
-    WILDCARD_FILTER = "[^\\.]+",
-
-    // a wildcard filter which captures the value;
-    // used in a relaxed filter expression to capture the filter value
-    CAPTURE_WILDCARD_FILTER = "(" + WILDCARD_FILTER + ")",
-
-    // character used as separator between filters in the filter expression
-    FILTER_SEPARATOR = "\\.",
-
-    // start of the filter expression:
-    // anchor the expression to the start of the string
-    FILTER_START = "^",
-
-    // end of the filter expression:
-    // add the 'x' character before the multiplier,
-    // then capture the remaining characters in group CAPTURED_MULTIPLIER.
-    FILTER_END = "x(.+)$";
+    AUTHOR_CONTRIBUTIONS = this.AUTHOR_CONTRIBUTIONS ,
+    CONTRIBUTION_CODE_FILTERS = this.CONTRIBUTION_CODE_FILTERS ,
+    TOTAL_CONTRIBUTIONS_FILTER = this.TOTAL_CONTRIBUTIONS_FILTER,
+    WILDCARD_FILTER = this.WILDCARD_FILTER,
+    CAPTURE_WILDCARD_FILTER = this.CAPTURE_WILDCARD_FILTER,
+    FILTER_SEPARATOR = this.FILTER_SEPARATOR,
+    FILTER_START = this.FILTER_START,
+    FILTER_END = this.FILTER_END;
 
   function getTotalMatchingContributions(author, filterRegExp) {
     var
---
+++ code/filter-constants.js
@@ -0,0 +1,42 @@
+within("projetmedea.fr", function() {
+
+  // offset of the field with the list of author contribution codes
+  // in each author record
+  this.AUTHOR_CONTRIBUTIONS = 3;
+
+  // sequence of the names of filters
+  // that make up the contribution code
+  this.CONTRIBUTION_CODE_FILTERS = [
+    "assessment-report-categories",
+    "working-group-categories",
+    "role-categories",
+    "country-categories",
+    "institution-type-categories",
+    "institution-categories"
+  ];
+
+  // name of the filter for the total number of contributions
+  // corresponding to the contribution code
+  this.TOTAL_CONTRIBUTIONS_FILTER = "total-contributions-categories";
+
+  // part of the filter expression for a filter that matches any value
+  // (matches all characters up to the next filter separator)
+  this.WILDCARD_FILTER = "[^\\.]+";
+
+  // a wildcard filter which captures the value;
+  // used in a relaxed filter expression to capture the filter value
+  this.CAPTURE_WILDCARD_FILTER = "(" + this.WILDCARD_FILTER + ")";
+
+  // character used as separator between filters in the filter expression
+  this.FILTER_SEPARATOR = "\\.";
+
+  // start of the filter expression:
+  // anchor the expression to the start of the string
+  this.FILTER_START = "^";
+
+  // end of the filter expression:
+  // add the 'x' character before the multiplier,
+  // then capture the remaining characters in group CAPTURED_MULTIPLIER.
+  this.FILTER_END = "x(.+)$";
+
+});
