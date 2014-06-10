c013b096aacd2bb83dbd322525a8b1ea63db0974
Eric Bréchemier <medea@eric.brechemier.name>
2014-06-10 09:11:58 +0200

    Add TODO comments about use of CSS3 transitions for fadeIn/fadeOut

--- code/display-author-card.js
+++ code/display-author-card.js
@@ -1,15 +1,22 @@
 within("projetmedea.fr", function(publish, subscribe) {
   var
+    // addClass = this.addClass,
+    // removeClass = this.removeClass,
+
     showElement = this.showElement,
     hideElement = this.hideElement,
 
     authorCard = document.getElementById( 'author-card' );
 
   function showAuthorCard() {
+    // TODO: define CSS3 fadeIn transition
+    // addClass( authorCard, 'fade-in' );
     showElement( authorCard );
   }
 
   function hideAuthorCard() {
+    // TODO: define CSS3 fadeOut transition
+    // addClass( authorCard, 'fade-out' );
     hideElement( authorCard );
   }
 
