865bb2e34d304c1640df90313cb06ec0933da61d
Eric Bréchemier <medea@eric.brechemier.name>
2014-06-11 19:37:05 +0200

    Set the position of the author card below the hovered tile

--- code/display-author-card.js
+++ code/display-author-card.js
@@ -6,9 +6,27 @@ within("projetmedea.fr", function(publish, subscribe) {
     showElement = this.showElement,
     hideElement = this.hideElement,
 
-    authorCard = document.getElementById( 'author-card' );
+    authorCard = document.getElementById( 'author-card' ),
 
-  function showAuthorCard() {
+    // margin between a tile and the top of the card, in pixels
+    CARD_TOP_MARGIN = 20;
+
+  function getTopPosition( node ) {
+    var boundingBox = node.getBoundingClientRect();
+    // the bounding box is relative to the viewport, not the page
+    return window.pageYOffset + boundingBox.top;
+  }
+
+  function setTopPosition( node, topPosition ) {
+    node.style.top = topPosition + "px";
+  }
+
+  function showAuthorCard( tileNode ) {
+    setTopPosition(
+      authorCard,
+      getTopPosition( tileNode ) + CARD_TOP_MARGIN
+    );
+
     // TODO: define CSS3 fadeIn transition
     // addClass( authorCard, 'fade-in' );
     showElement( authorCard );
